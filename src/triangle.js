2fc10cd164b7e2c1099e49d03930aa5abbcf0b46
Sean Downs <spdowns@protonmail.com>
2020-06-23 15:56:37 -0700

    add console.log and debugger practice
    Co-Authored-By: James Henager <jameschenager@gmail.com>

--- src/triangle.js
+++ src/triangle.js
@@ -1,19 +1,20 @@
 export function Triangle(side1, side2, side3) {
+  console.log(side1, side2, side3)
   this.side1 = side1;
   this.side2 = side2;
-  this.side3 = side3;
+  this.side3 = side2;
 }
 
 Triangle.prototype.checkType = function() {
-  if (isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) {
+    if (isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) {
     return "Words Only";
   } else if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
-  } else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
-    return "equilateral triangle";
-  } else if ((this.side1 !== this.side2) && (this.side1 !== this.side3) && (this.side2 !== this.side3)) {
+  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  } else if ((this.side1 === this.side2) || (this.side1 === this.side3) || (this.side2 === this.side3)) {
-    return "isosceles triangle";
+  }  else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
+    return "equilateral triangle";
+  } else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+      return "isosceles triangle";
   }
 };
